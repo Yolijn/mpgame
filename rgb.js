fcadc2bf55c9e6aa9dbab5cef8cd1ff7589a0691
Yolijn van der Kolk <mail@yolijn.com>
2017-10-17 19:01:26 +0200

    Use spaces

--- rgb.js
+++ rgb.js
@@ -1,28 +1,28 @@
 const { randomInt } = require('./math.js');
 
 function randomRGBValue() {
-	return randomInt(0, 255);
+    return randomInt(0, 255);
 }
 
 function isRGBValue(value) {
-	return value >= 0 && value <= 255;
+    return value >= 0 && value <= 255;
 }
 
 class RGB {
-	constructor(r, g, b) {
-		this.r = isRGBValue(r) ? r : randomRGBValue();
-		this.g = isRGBValue(g) ? g : randomRGBValue();
-		this.b = isRGBValue(b) ? b : randomRGBValue();
-	}
+    constructor(r, g, b) {
+        this.r = isRGBValue(r) ? r : randomRGBValue();
+        this.g = isRGBValue(g) ? g : randomRGBValue();
+        this.b = isRGBValue(b) ? b : randomRGBValue();
+    }
 
-	toString() {
-		return `rgb(${r}, ${g}, ${b })`;
-	}
+    toString() {
+        return `rgb(${r}, ${g}, ${b })`;
+    }
 }
 
 /*
-	This is a module for creating and saving RGB values
-	translating them to a string
-	creating a random rgb instance
+    This is a module for creating and saving RGB values
+    translating them to a string
+    creating a random rgb instance
 */
 module.exports = RGB;
